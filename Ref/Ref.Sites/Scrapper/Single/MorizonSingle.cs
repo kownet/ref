b301cea71cfa602b90be9712c601f948612b1b37
tkowalczyk <tomek@kownet.info>
2019-03-31 19:36:28 +0200

    morizonsingle

--- Ref/Ref.Sites/Scrapper/Single/MorizonSingle.cs
+++ Ref/Ref.Sites/Scrapper/Single/MorizonSingle.cs
@@ -1,12 +1,15 @@
 ﻿using Ref.Data.Models;
 using Ref.Shared.Providers;
 using Ref.Sites.Helpers;
+using ScrapySharp.Extensions;
+using System.Linq;
 
 namespace Ref.Sites.Scrapper.Single
 {
     public class MorizonSingle : SingleSiteToScrapp, ISingleSiteToScrapp
     {
-        public MorizonSingle(IAppScrapperProvider appProvider) : base(appProvider)
+        public MorizonSingle(IAppScrapperProvider appProvider)
+            : base(appProvider)
         {
         }
 
@@ -20,6 +23,25 @@ namespace Ref.Sites.Scrapper.Single
                 return result;
 
             var doc = scrap.HtmlNode;
+
+            var archive = doc.CssSelect(".exclamation").FirstOrDefault();
+
+            if (!(archive is null))
+            {
+                result.IsDeleted = true;
+            }
+            else
+            {
+                var content = doc.CssSelect(".description").FirstOrDefault();
+
+                if (!(content is null))
+                {
+                    if (!string.IsNullOrWhiteSpace(content.InnerText))
+                    {
+                        result.Content = content.InnerText.Trim();
+                    }
+                }
+            }
 
             return result;
         }
