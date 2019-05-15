941a74573725c5938ed76bcbdc08773615521405
tkowalczyk <tomek@kownet.info>
2019-05-15 21:06:42 +0200

    only active sites in events

--- Ref/Ref.Services/Features/Queries/Events/All.cs
+++ Ref/Ref.Services/Features/Queries/Events/All.cs
@@ -1,4 +1,5 @@
 ﻿using MediatR;
+using Ref.Data.Models;
 using Ref.Data.Repositories;
 using Ref.Services.Features.Shared;
 using Ref.Shared.Extensions;
@@ -27,22 +28,33 @@ namespace Ref.Services.Features.Queries.Events
         public class Handler : IRequestHandler<Query, Result>
         {
             private readonly IEventRepository _eventRepository;
+            private readonly ISiteRepository _siteRepository;
 
-            public Handler(IEventRepository eventRepository)
+            public Handler(
+                IEventRepository eventRepository,
+                ISiteRepository siteRepository)
             {
                 _eventRepository = eventRepository;
+                _siteRepository = siteRepository;
             }
 
             public async Task<Result> Handle(Query request, CancellationToken cancellationToken)
             {
                 try
                 {
+                    var activeSites = await _siteRepository.FindByAsync(s => s.IsActive);
+
                     var entities = await _eventRepository.GetAllAsync();
                     var result = new List<EventResult>();
 
-                    if (entities.AnyAndNotNull())
+                    var activeEvents = new List<Event>();
+
+                    activeEvents.AddRange(entities.Where(e => activeSites.Any(a => (int)a.Type == (int)e.Category)));
+                    activeEvents.AddRange(entities.Where(e => activeSites.Any(a => (int)a.Type == ((int)e.Category - 100))));
+
+                    if (activeEvents.AnyAndNotNull())
                     {
-                        foreach (var entity in entities)
+                        foreach (var entity in activeEvents)
                         {
                             result.Add(new EventResult
                             {
